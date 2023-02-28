32aef61b395de6bb381485c1286a5bcca90ae95e
Shiva Kumar H R <shivahr@gmail.com>
2023-02-28 13:50:58 +0530

    Customize for JnanaVahini

--- docker/init.sh
+++ docker/init.sh
@@ -1,10 +1,15 @@
-
 echo "START ENV SUBSTITUTION" && envsubst < /tmp/config.yaml > /config.yaml && echo "END ENV SUBSTITUTION"
 
 # Update base href of frontend index.html
-sed -i "s@base href=\"/\"@base href=\"${BASE_URL}\"@g" $1/index.html
+if [ -n "$2" ]
+then
+  sed -i "s@base href=\"/\"@base href=\"${BASE_URL}\"@g" $2/index.html
+fi
 
 mkdir -p $LCP_DATABASE_ROOT_PATH
 mkdir -p $LCP_STORAGE_PATH
 mkdir -p $FRONTEND_MASTER_PATH
 mkdir -p $FRONTEND_ENCRYPTED_PATH
+
+echo "exec $1"
+exec $1
