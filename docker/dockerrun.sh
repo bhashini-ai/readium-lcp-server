ed55a8825dbd510bbbfcb0a2afbe2f8440ba18d9
Shiva Kumar H R <shivahr@gmail.com>
2023-02-28 17:05:13 +0530

    Start various dockers

--- docker/dockerrun.sh
+++ docker/dockerrun.sh
@@ -1,17 +1,15 @@
 #! /bin/bash
 
-PORT=8080
+docker stop lcpserver
+docker rm lcpserver
+docker run -d --restart always --publish 8989:8989 --volume /home/lcp:/lcp --name lcpserver lcpserver
 
-docker kill mylcp
-docker rm mylcp
+# docker run --rm -it --entrypoint sh lcpserver #For debugging
 
-# uncomment this line to build
-# ./docker/dockerbuild.sh `pwd` master
+docker stop lsdserver
+docker rm lsdserver
+docker run -d --restart always --publish 8990:8990 --volume /home/lcp:/lcp --name lsdserver lsdserver
 
-docker run \
-  --name mylcp \
-  --publish $PORT:$PORT \
-  --env PORT=$PORT \
-  --volume /tmp/lcp:/lcp \
-  --platform linux/amd64 \
-  lcpmasterserver
+docker stop frontendtestserver
+docker rm frontendtestserver
+docker run -d --restart always --publish 8991:8991 --volume /home/lcp:/lcp --name frontendtestserver frontendtestserver
