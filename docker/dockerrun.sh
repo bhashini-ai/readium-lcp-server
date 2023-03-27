7a65a3669bb0d8c556a7a06589522f5e942ac119
Shiva Kumar H R <shivahr@gmail.com>
2023-03-27 18:10:52 +0530

    Bug fix

--- docker/dockerrun.sh
+++ docker/dockerrun.sh
@@ -2,8 +2,8 @@
 
 docker stop lcpserver
 docker rm lcpserver
-docker run -d --restart always --publish $LCP_PORT:$LCP_PORT --volume $LCP_VOLUME_DIR:/lcp -e AUTH_FILE=/lcp/htpasswd -e CERTIFICATE_PATH=/lcp/cert/cert.pem -e PRIVATE_KEY_PATH=/lcp/cert/privkey.pem -e LCP_STORAGE_PATH=/lcp/files/storage -e LCP_HOST -e LCP_PORT -e LSD_HOST -e LSD_PORT -e LCP_BASE_URL -e LCP_UPDATE_AUTH_USER -e LCP_UPDATE_AUTH_PASS -e LSD_BASE_URL -e LSD_NOTIFY_AUTH_USER -e LSD_NOTIFY_AUTH_PASS -e PUBLIC_URL --name lcpserver lcpserver
+docker run -d --restart always --publish $LCP_PORT:$LCP_PORT --volume $LCP_VOLUME_DIR:/lcp -e AUTH_FILE=/lcp/htpasswd -e CERTIFICATE_PATH=/lcp/cert/cert.pem -e PRIVATE_KEY=/lcp/cert/privkey.pem -e LCP_STORAGE_PATH=/lcp/files/storage -e LCP_HOST -e LCP_PORT -e LSD_HOST -e LSD_PORT -e LCP_BASE_URL -e LCP_UPDATE_AUTH_USER -e LCP_UPDATE_AUTH_PASS -e LSD_BASE_URL -e LSD_NOTIFY_AUTH_USER -e LSD_NOTIFY_AUTH_PASS -e PUBLIC_URL --name lcpserver lcpserver
 
 docker stop lsdserver
 docker rm lsdserver
-docker run -d --restart always --publish $LSD_PORT:$LSD_PORT --volume $LCP_VOLUME_DIR:/lcp -e AUTH_FILE=/lcp/htpasswd -e CERTIFICATE_PATH=/lcp/cert/cert.pem -e PRIVATE_KEY_PATH=/lcp/cert/privkey.pem -e LCP_STORAGE_PATH=/lcp/files/storage -e LCP_HOST -e LCP_PORT -e LSD_HOST -e LSD_PORT -e LCP_BASE_URL -e LCP_UPDATE_AUTH_USER -e LCP_UPDATE_AUTH_PASS -e LSD_BASE_URL -e LSD_NOTIFY_AUTH_USER -e LSD_NOTIFY_AUTH_PASS -e PUBLIC_URL --name lsdserver lsdserver
+docker run -d --restart always --publish $LSD_PORT:$LSD_PORT --volume $LCP_VOLUME_DIR:/lcp -e AUTH_FILE=/lcp/htpasswd -e CERTIFICATE_PATH=/lcp/cert/cert.pem -e PRIVATE_KEY=/lcp/cert/privkey.pem -e LCP_STORAGE_PATH=/lcp/files/storage -e LCP_HOST -e LCP_PORT -e LSD_HOST -e LSD_PORT -e LCP_BASE_URL -e LCP_UPDATE_AUTH_USER -e LCP_UPDATE_AUTH_PASS -e LSD_BASE_URL -e LSD_NOTIFY_AUTH_USER -e LSD_NOTIFY_AUTH_PASS -e PUBLIC_URL --name lsdserver lsdserver
